2cb94fdbe370638a271f4891dadac44554074b9f
Back <yhf_szb@qq.com>
2015-12-26 02:31:53 +0800

    修改了服务配置默认名称。

--- src/Config/Services.php
+++ src/Config/Services.php
@@ -4,8 +4,8 @@ $servicesNamespace = '\DIServer\Services';
 $ifaceNamespace = '\DIServer\Interfaces';
 //这里提供服务的基本映射
 return [
-    $ifaceNamespace . '\IBootstrapper' => $servicesNamespace . '\Bootstrapper',
-    $ifaceNamespace . '\IProcessManager' => 'DIServer\ProcessManager',
+//    $ifaceNamespace . '\IBootstrapper' => $servicesNamespace . '\Bootstrapper',
+//    $ifaceNamespace . '\IProcessManager' => 'DIServer\ProcessManager',
     //swoole服务注册
     $ifaceNamespace . '\IMasterServer' => $servicesNamespace . '\MasterServer',
     $ifaceNamespace . '\IManagerServer' => $servicesNamespace . '\ManagerServer',
