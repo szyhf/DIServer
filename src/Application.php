10193386e3e2d92e2d270d994de0381cf7970f51
Back <yhf_szb@qq.com>
2015-12-25 20:05:57 +0800

    更新了程序入口。

---
+++ src/Application.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace DIServer;
+
+use \DIServer\DI\DIContainer as Container;
+use \DIServer\Interfaces\IBootstrapper as IBootstrapper;
+use \DIServer\Services\Bootstrapper as Bootstrapper;
+
+/**
+ * 主程序
+ * @author Back
+ */
+class Application
+{
+
+    /**
+     * 默认容器
+     * @var \DIServer\DI\DIContainer
+     */
+    protected $ioc;
+
+    /**
+     * 项目目录
+     * @var string 
+     */
+    protected $basePath;
+
+    /**
+     * 环境目录
+     * @var string 
+     */
+    protected $envermentPath;
+
+    public function __construct(Container $container, $basePath = __DIR__)
+    {
+	$this->ioc = $container;
+	$this->registerBaseClass();
+	$this->registerBaseServiceProviders();
+//	$this->registerCoreContainerAliases();
+	if ($basePath)
+	{
+	    $this->setBasePath($basePath);
+	}
+    }
+
+    /**
+     * 获取当前容器
+     * @return \DIServer\DI\DIContainer
+     */
+    public function IOC()
+    {
+	return $this->ioc;
+    }
+
+    protected function registerBaseClass()
+    {
+	
+    }
+
+    protected function registerBaseServiceProviders()
+    {
+//	var_dump(interface_exists(IBootstrapper::class));
+	$this->IOC()->RegisterClass(Bootstrapper::class);
+	$this->IOC()->RegisterInterfaceByClass(IBootstrapper::class, Bootstrapper::class);
+    }
+
+    protected function registerCoreContainerAliases()
+    {
+	
+    }
+
+    public function boot()
+    {
+	/* @var $bootstrapper \DIServer\Interfaces\IBootstrapper */
+	$bootstrapper = $this->IOC()->GetInstance(IBootstrapper::class);
+	$bootstrapper->Boot();
+    }
+
+    /**
+     * Set the base path for the application.
+     *
+     * @param  string  $basePath
+     * @return $this
+     */
+    public function SetBasePath($basePath)
+    {
+	$this->basePath = realpath(rtrim($basePath, '\/'));
+
+//        $this->bindPathsInContainer();
+
+	return $this;
+    }
+
+    public function GetBasePath()
+    {
+	return $this->basePath;
+    }
+
+}
