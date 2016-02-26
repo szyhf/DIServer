e12b3c0e76102c7af060c8343f2a8bc8b137100b
szyhf <szyhf@163.com>
2016-02-26 13:43:36 +0800

    移除了Services配置。 更新了默认Swoole配置。

--- src/Config/Swoole.php
+++ src/Config/Swoole.php
@@ -14,17 +14,17 @@ return [
 	'task_worker_num'          => 2, //配置task进程的数量，配置此参数后将会启用task功能（DIServer必须开启此功能）。
 	'task_ipc_mode'            => 2, //设置task进程与worker进程之间通信的方式，1使用unix socket通信，2使用消息队列通信，3使用消息队列通信，并设置为争抢模式
 	'task_max_request'         => 10, //task进程的最大任务数,不希望进程自动退出可以设置为0
-	'task_tmpdir'              => DI_APP_SERVER_TEMP_PATH, //设置task的数据临时目录，在swoole_server中，如果投递的数据超过8192字节，将启用临时文件来保存数据。
+	//'task_tmpdir'              => DI_APP_SERVER_TEMP_PATH, //设置task的数据临时目录，在swoole_server中，如果投递的数据超过8192字节，将启用临时文件来保存数据。
 	'dispatch_mode'            => 2, //1轮循模式，2固定模式，3抢占模式，4IP分配，5UID分配。
-	'message_queue_key'        => ftok(DI_APP_SERVER_PATH, 3), //设置消息队列的KEY
+	//'message_queue_key'        => ftok(DI_APP_SERVER_PATH, 3), //设置消息队列的KEY
 	'daemonize'                => DI_DAEMONIZE,//是否作为守护进程
 	'backlog'                  => 128, //Listen队列长度，此参数将决定最多同时有多少个等待accept的连接。
-	'log_file'                 => DI_LOG_PATH . '/' . DI_LOG_FILE_NAME, //指定swoole的日志文件（启用守护进程后，标准输入和输出会被重定向到 log_file）。
+	//'log_file'                 => DI_LOG_PATH . '/' . DI_LOG_FILE_NAME, //指定swoole的日志文件（启用守护进程后，标准输入和输出会被重定向到 log_file）。
 	'heartbeat_check_interval' => 60, //启用心跳检测，此选项表示每隔多久轮循一次，单位为秒。
 	'heartbeat_idle_time'      => 120, //表示连接最大允许空闲（没消息）的时间
-	'open_eof_check'           => false, //打开EOF检测，此选项将检测客户端连接发来的数据，当数据包结尾是指定的字符串时才会投递给Worker进程。
-	'open_eof_split'           => false, //启用后，底层会从数据包中间查找EOF，并拆分数据包。
-	'package_eof'              => '', //与open_eof_check配合使用，设置EOF字符串。
+	'open_eof_check'           => '', //打开EOF检测，此选项将检测客户端连接发来的数据，当数据包结尾是指定的字符串时才会投递给Worker进程。
+	'open_eof_split'           => '', //启用后，底层会从数据包中间查找EOF，并拆分数据包。
+	'package_eof'              => '0', //与open_eof_check配合使用，设置EOF字符串。
 	'open_length_check'        => true, //打开包长检测特性。包长检测提供了固定包头+包体这种格式协议的解析。
 	'package_length_type'      => 'L', //长度值的类型，接受一个字符参数，与php的pack函数一致。
 	'package_max_length'       => 1024 * 1024 * 10, //一个数据包最大允许占用的内存尺寸，超出的数据包会被丢弃。
@@ -33,7 +33,7 @@ return [
 	'cpu_affinity_ignore'      => '', //接受一个数组作为参数，array(0, 1) 表示不使用CPU0,CPU1，专门空出来处理网络中断。
 	'open_tcp_nodelay'         => true, //启用open_tcp_nodelay，开启后TCP连接发送数据时会关闭Nagle合并算法，立即发往客户端连接。
 	'tcp_defer_accept'         => '', //用处不明
-	'ssl_cert_file'            => '', //设置SSL隧道加密，设置值为一个文件名字符串，制定cert证书和key的路径。
+	//'ssl_cert_file'            => '', //设置SSL隧道加密，设置值为一个文件名字符串，制定cert证书和key的路径。
 	'user'                     => '', //设置worker/task子进程的所属用户。
 	'group'                    => '', //设置worker/task子进程的进程用户组。
 	//'chroot'                   => DI_APP_PATH, //重定向Worker进程的文件系统根目录（会影响autoloader，慎用）
@@ -42,5 +42,5 @@ return [
 	'enable_unsafe_event'      => false,
 	//在配置dispatch_mode=1或3后，因为系统无法保证onConnect/onReceive/onClose的顺序，默认关闭了onConnect/onClose事件。
 	'discard_timeout_request'  => true, //为true，表示如果worker进程收到了已关闭连接的数据请求，将自动丢弃。
-	'enable_reuse_port'        => false//启用端口重用后可以重复启动同一个端口的Server程序
+	'enable_reuse_port'        => true//启用端口重用后可以重复启动同一个端口的Server程序
 ];
